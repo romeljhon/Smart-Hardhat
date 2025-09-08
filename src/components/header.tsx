c79bcba593a5ab0ea1aa19d65162c0d566ba1210
romeljhon salvaleon <pajojhon@gmail.com>
2025-09-08 01:22:07 +0000

    how to back in homepage?

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,7 +7,7 @@ export function Header() {
     <header className="sticky top-0 z-50 flex h-16 items-center justify-between gap-4 border-b border-b-border/40 bg-background/95 px-4 backdrop-blur-sm md:px-6">
       <nav className="flex flex-row items-center gap-6 text-lg font-medium md:text-sm">
         <Link
-          href="#"
+          href="/"
           className="flex items-center gap-2 text-lg font-semibold md:text-base"
         >
           <Logo className="h-7 w-7 text-primary" />
